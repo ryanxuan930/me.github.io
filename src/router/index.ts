230e310575eb980bb48f4c00f0b86ae5e4a53561
Ryan P. H. Chang <anakinchung930@gmail.com>
2024-04-08 22:34:50 +0800

    fix bugs

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory((import.meta as any).env.BASE_URL),
   routes: [
     {
       path: '/',
